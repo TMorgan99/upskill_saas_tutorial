a5c571c3573456f867fd0e4a5a6494c4f4fced46
Tim Morgan <tmorgan99@gmail.com>
2016-12-03 07:58:02 +0000

    Added Mailgun settings to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+herokuapp = 'boiling-beyond-48073'
+
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => "#{herokuapp}.herokuapp.com",
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
