b50231ed10851209c98099e664f4cc94f77f7c5d
Tim Morgan <tmorgan99@gmail.com>
2016-12-07 06:27:14 +0000

    Added params for Membership Plans in the Home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    # do we need to fetch data just to get this param?
+    # @basic = MembershipPlan.find(1)
+    # @pro = MembershipPlan.find(2)
   end
 
   def about
