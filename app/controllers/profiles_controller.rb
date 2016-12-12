bf90e417069e66e0cd5c109eff57c42486805a67
Tim Morgan <tmorgan99@gmail.com>
2016-12-12 05:12:23 +0000

    Added Paperclip upload functionality. (ignore public/system storage)

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -26,6 +26,7 @@ class ProfilesController < ApplicationController
   def profile_params
     params.require(:profile).permit(
       :first_name, :last_name, :job_title, :phone_number, :contact_email, :description,
+      :avatar
       )
   end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,9 +1,10 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :avatar,
-    default_url: 'images/:style/missing.png',
-    :styles => { medium: '300x300>', thumb: '100x100>' }
+
+  has_attached_file :avatar, 
+    default_url: '/images/:style/missing.png',
+    styles: { medium: '300x300>', thumb: '100x100>' } 
     
-  validates_attachment_content_type :avatar, content_type: /\Aimage\.*\Z/
-  
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
