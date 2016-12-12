8c537867732ce7b88bf4400fb76a032098b94183
Tim Morgan <tmorgan99@gmail.com>
2016-12-12 00:59:59 +0000

    Added Profile#create

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,4 +5,25 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   
+  def create
+    # what if somebody hacks the URL?
+    # why not save to 'current_user'
+    @user = User.find params[ :user_id ]
+    @profile = @user.build_profile profile_params
+    if @profile.save
+      flash[ :success ] = "Profile updated!"
+      redirect_to root_path
+    else
+      # ? redirect and flash.
+      render action: :new
+    end
+  end
+  
+  
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :first_name, :last_name, :job_title, :phone_number, :contact_email, :description,
+      )
+  end
 end
