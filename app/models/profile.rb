927fcb9349c3084a29daef0562777f2f12501842
Tim Morgan <tmorgan99@gmail.com>
2016-12-12 04:01:53 +0000

    Added Paperclip Avatar to Profile table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,9 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :avatar,
+    default_url: 'images/:style/missing.png',
+    :styles => { medium: '300x300>', thumb: '100x100>' }
+    
+  validates_attachment_content_type :avatar, content_type: /\Aimage\.*\Z/
   
 end
