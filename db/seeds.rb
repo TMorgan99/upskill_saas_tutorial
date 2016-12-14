54da4b796f77bb17a5499166218a8b4fa0c7e44e
Tim Morgan <tmorgan99@gmail.com>
2016-12-14 03:26:44 +0000

    Improvements to Home Page

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,24 @@
 
 MembershipPlan.create name: 'basic', price: 0
 MembershipPlan.create name: 'pro', price: 9.95
+
+# create the first users....
+
+# u1 = User.create
+# Profile.create user_id: u1,    first_name: 'Bob',   last_name:  'Crow',    job_title:  'Investor',
+    # avatar_file_name
+    # membership_plan_id: 1,
+    # stripe_customer_token: 
+    
+
+# u2 = User.create
+# Profile.create user_id: u2,    first_name: 'Nancy', last_name:  'Crow',    job_title:  'Developer'
+
+# u3 = User.create
+# Profile.create user_id: u3,    first_name: 'Hank',  last_name:  'Gilbert', job_title:  'Entrepreneur'
+
+#     stripe_customer_token: 'cus_9jwe0WcuLmXEt1'
+
+
+
+
